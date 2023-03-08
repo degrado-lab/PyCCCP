72c050e150b5d17c68884fc32b82608da4af11a7
Rian Kormos <40475814+rckormos@users.noreply.github.com>
2023-03-07 23:29:04 -0800

    Added numpy import to PDBIO.py

--- pycccp/PDBIO.py
+++ pycccp/PDBIO.py
@@ -12,6 +12,8 @@
 #   You should have received a copy of the GNU General Public License along 
 #   with this program. If not, see <http://www.gnu.org/licenses/>
 
+import numpy as np
+
 from Bio.PDB.PDBIO import PDBIO
 from Bio.PDB.Atom import Atom
 from Bio.PDB.Residue import Residue
